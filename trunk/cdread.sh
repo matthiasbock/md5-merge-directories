97b898851d2a667f0611a786eabaf908ab0502ef
matthias.bock@hu-berlin.de <matthias.bock@hu-berlin.de@905f6435-bd35-4ded-1f63-3ce319a20962>
2011-10-31 14:11:40 +0000

    cdread enhanced

--- trunk/cdread.sh
+++ trunk/cdread.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 
+dev=$1
+saveto=$2
+
 $(espeak -v de "CD wird eingelesen. Bitte warten." &> /dev/zero) &
-rmdir copy
-umount -fl /tmp/mount
+
+# clean up
+rmdir $saveto
+umount -fl "/tmp/$dev"
 sleep 2
-rmdir /tmp/mount
-rm /tmp/ddrescue.iso
-ddrescue /dev/cdrom1 /tmp/ddrescue.iso --max-retries=1 --no-split --verbose
-umount -fl /dev/cdrom1
+rmdir "/tmp/$dev"
+rm /tmp/ddrescue-$dev.iso
+
+# rescue
+ddrescue /dev/$dev /tmp/ddrescue-$dev.iso --max-retries=1 --no-split --verbose
+mv /tmp/ddrescue-$dev.iso /tmp/ddrescue-$dev-finished.iso
+chown user.user /tmp/ddrescue-$dev-finished.iso
+chmod 644 /tmp/ddrescue-$dev-finished.iso
+
+# finish
 $(espeak -v de "Einlesen abgeschlossen. CD wird ausgeworfen." &> /dev/zero) &
+umount -fl /dev/$dev
 sleep 2
-eject /dev/cdrom1
+eject /dev/$dev
+
+# move data
 $(espeak -v de "Transferiere die Daten aus dem Abbild auf die Festplatte. Bitte warten." &> /dev/zero) &
-chown user.user /tmp/ddrescue.iso
-chmod 644 /tmp/ddrescue.iso
-mv /tmp/ddrescue.iso /tmp/ddrescue2.iso
-mkdir /tmp/mount
-mount /tmp/ddrescue2.iso /tmp/mount -o loop
-mount /tmp/ddrescue2.iso /tmp/mount -o loop -t iso9660
-if [ -e copy ]; then
-	cp /tmp/mount copy2 -paR
+mkdir "/tmp/$dev"
+mount /tmp/ddrescue-$dev-finished.iso "/tmp/$dev" -o loop -t auto
+mount /tmp/ddrescue-$dev-finished.iso "/tmp/$dev" -o loop -t iso9660
+if [ -e $saveto ]; then
+	cp "/tmp/$dev" "_$saveto" -paR
 else
-	cp /tmp/mount copy -paR
+	cp "/tmp/$dev" $saveto -paR
 fi
-umount -fl /tmp/mount
-rmdir /tmp/mount
-chown user.user copy -R
-chmod 755 copy -R
+umount -fl "/tmp/$dev"
+rmdir "/tmp/$dev"
+chown user.user $saveto -R
+chmod 755 $saveto -R
+
 $(espeak -v de "Datentransfehr abgeschlossen." &> /dev/zero) &
 
