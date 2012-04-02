8fa55e60417a4097fd99f964bd7614a13644754e
matthias.bock@hu-berlin.de <matthias.bock@hu-berlin.de@905f6435-bd35-4ded-1f63-3ce319a20962>
2012-04-02 22:22:09 +0000

    pack/unpack seems to work

--- trunk/scp.py
+++ trunk/scp.py
@@ -3,15 +3,17 @@
 
 from filesystem import exists, isdir
 
-def scp( source, target ):
-	s = source.split('/')
-	filename = s[len(s)-1]
-	if target.find(filename) == len(target)-len(filename):	# wrong:  scp host1:/abc/test.tar.bz host2:/abc/test.tar.bz
-		target = target[:-len(filename)]		# right:  scp host1:/abc/test.tar.bz host2:/abc/
-
-	print "scp -Cpr "+source+" "+target
+def scp( source, target, debug=False ):
 	from subprocess import Popen
-	Popen( ["scp", "-Cpr", source, target] ).wait()
-	if not exists(source) or not exists(target):
-		raise	# should be fatal, else the file may accidently be deleted permanently
+	from shlex import split
 
+	cmd = ['scp', '-Cpr', source, target.replace(' ', '\ ')]
+	if debug:
+		print ' '.join(cmd)
+	Popen(cmd).wait()
+
+	success = exists(source) and exists(target)
+	if debug:
+		print 'Success: '+str(success)
+
+	return success
