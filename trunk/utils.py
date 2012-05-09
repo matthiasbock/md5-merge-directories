9a281286914f2e778e7bb41d778499b2d676d5d6
matthias.bock@hu-berlin.de <matthias.bock@hu-berlin.de@905f6435-bd35-4ded-1f63-3ce319a20962>
2012-05-09 12:54:44 +0000

    partial md5sums

--- trunk/utils.py
+++ trunk/utils.py
@@ -12,3 +12,25 @@ def format(size):
 			break
 	return str(size)+' '+suffix
 
+def run(cmd):
+	from subprocess import Popen, PIPE
+	from shlex import split
+
+	print cmd
+
+	pipes = cmd.split('|')
+	if len(pipes) == 1:
+		return Popen(split(pipes[0]), stdout=PIPE).communicate()[0]
+	else:
+		p = [ Popen(split(pipes[0]), stdout=PIPE) ]
+		for i in range(1, len(pipes)):
+			p.append( Popen(split(pipes[i]), stdin=p[i-1].stdout, stdout=PIPE) )
+		return p[len(p)-1].communicate()[0]
+
+def ggT(a, b):
+	if a < b:
+		a, b = b, a
+	while a % b != 0:
+		a, b = b, a % b
+	return b
+
